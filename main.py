90e6ab63e5f26ee74421c13e1e3b23ea8171fe3c
Erik Montag <47230559+emontag47@users.noreply.github.com>
2021-11-09 13:21:35 -0500

    added first version of searching and excluding allergens

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """REST API for TragicElephant"""
 
-from fastapi import FastAPI, Body, HTTPException, status
+from fastapi import FastAPI, Body, HTTPException, status, Query
 from pymongo import MongoClient
 from pprint import pprint
 from fastapi.encoders import jsonable_encoder
@@ -28,7 +28,19 @@ mycol = mydb["Restaurants4"]
 
 # List of allergens to check against 
 allergens = ["milk", "eggs", "bass", "salmon", "cod", "crab", "lobster", "shrimp", "almonds", "walnuts", "pecans", "peanuts", "wheat", "soy", "ham", "bacon", "sausage", "steak", "chicken", "turkey", "cheese", "sunflower", "sesame", "poppy"]
-
+allergensDict = {
+  "dairy": ["milk", "yogurt", "butter", "cream", "mayonnaise"],
+  "cheese": ["mozzarella", "cheddar", "parmesan", "gouda", "swiss", "brie", "feta", "pepper jack", "montery jack", "provolone"],
+  "eggs": ["waffles", "pancakes", "egg", "eggnog", "mayonnaise"],
+  "fish": ["bass", "salmon", "tilapia", "cod"],
+  "shellfish": ["shrimp", "crab", "lobster", "crayfish"],
+  "nuts": ["peanuts", "almonds", "cashews", "pecans"],
+  "soy": ["tofu"],
+  "meat": ["chicken", "steak", "pepperoni", "turkey", "bacon", "pork", "ribs"],
+  "sunflower": [],
+  "sesame": [],
+  "poppy": [],
+}
 # Models to handle BSON data returned by MongoDB
 class PyObjectId(ObjectId):
     @classmethod
@@ -83,28 +95,60 @@ def get_list_of_restaurants():
 @app.get("/api/{restaurant}/", response_description="Show meals from searched restaurant", response_model=OneRestaurantModel)
 def get_restaurant(restaurant: str):
     """Returns the restaurant name, meals, and potential allergens of the searched restaurant"""
-    print("HELLO")
     # Return 404 if the restaurant is not in our database
     if (x := mycol.find_one({"restaurant_name": { "$regex": restaurant, "$options" :'i' }}, {"_id": 0})) is None:
       raise HTTPException(status_code=404, detail=f"Restaurant {restaurant} not found")
 
     # Sort through data from the database to format data and return info above
-    meals = {}
-    for a in x["menus"]:
-      for b in a["menu_sections"]:
-        for i in b["menu_items"]:
-          newMeal = i["name"].lower()
-          newDesc = i["description"].lower()
-          fullMeal = "{} {}".format(newMeal, newDesc)
-          meals[newMeal] = {"allergens": [], "description": newDesc}
-          for allergen in allergens:
-            if fullMeal.find(allergen) > -1:
-              meals[newMeal]["allergens"].append(allergen)
-    
-    info = {
-      "restaurant_name": x["restaurant_name"],
-      "meals": meals
-    }
-    # print(info)
+    info = get_restaurant_helper(x)
 
     return info
+
+@app.get("/api/{restaurant}/allergy")
+def get_restaurant_allergy(restaurant: str, q: Optional[List[str]] = Query(None)):
+  """Returns meals from restaurant that don't contain selectedd allergens"""
+  
+  if (x := mycol.find_one({"restaurant_name": { "$regex": restaurant, "$options" :'i' }}, {"_id": 0})) is None:
+      raise HTTPException(status_code=404, detail=f"Restaurant {restaurant} not found")
+
+  info = get_restaurant_helper(x)
+  
+  meals = info["meals"]
+
+  for meal in list(meals.keys()):
+    foundAllergen = False
+    allergens = meals[meal]["allergens"]
+    for i in q:
+      if i in allergens:
+        foundAllergen = True
+    if foundAllergen:
+      del meals[meal] 
+
+  # for i in q:
+  #   final.append(i)
+  return info
+
+def get_restaurant_helper(restaurant):
+
+  # Sort through data from the database to format data and return info above
+  meals = {}
+  for a in restaurant["menus"]:
+    for b in a["menu_sections"]:
+      for i in b["menu_items"]:
+        newMeal = i["name"]
+        newDesc = i["description"]
+        fullMeal = "{} {}".format(newMeal.lower(), newDesc.lower())
+        meals[newMeal] = {"allergens": [], "description": newDesc}
+        for allergen in allergensDict:
+          if allergen in fullMeal:
+            meals[newMeal]["allergens"].append(allergen)
+          for i in allergensDict[allergen]:
+            if i in fullMeal:
+              meals[newMeal]["allergens"].append(allergen)
+  
+  info = {
+      "restaurant_name": restaurant["restaurant_name"],
+      "meals": meals
+    }
+
+  return info
